7ab35b80aec156f8b03229ca3f62cd5f6497716e
蛮吉 <hanabi.almost@gmail.com>
2026-08-26 15:54:42 +0800

    chore(release): 更新版本至 0.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "app.smartisanmusic.revived"
         minSdk = 27
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.2.1"
+        versionCode = 5
+        versionName = "0.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
